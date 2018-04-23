3602d83d868892d1c1594e68d62cc74d5df458cd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿#region Using
+using System;
+using System.Collections;
+using System.IO;
+using System.Xml;
+using DevExpress.Web.ASPxGridView;
+using System.Web.UI.WebControls;
+#endregion
+
+public partial class _Default : System.Web.UI.Page {
+    protected readonly static object lockObject = new object();
+    protected void ASPxGridView1_CellEditorInitialize(object sender, ASPxGridViewEditorEventArgs e) {
+        e.Editor.ReadOnly = false;
+    }
+    protected void ASPxGridView1_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        if (e.Parameters == "Restore") {
+            ASPxGridView grid = (ASPxGridView)sender;
+            RestoreXMLFile(grid);
+        }
+    }
+    protected void ASPxGridView1_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e) {
+        throw new InvalidOperationException("Data modifications are not allowed in online demos");
+
+        int maxCategoryID = 0;
+
+        foreach (XmlNode selectedNode in XmlDataSource1.GetXmlDocument().SelectNodes("Categories/Category")) {
+            int categoryID = int.Parse(selectedNode.Attributes["CategoryID"].Value);
+            if (categoryID > maxCategoryID)
+                maxCategoryID = categoryID;
+        }
+
+        e.NewValues.Insert(0, "CategoryID", maxCategoryID + 1);
+
+        XmlNode node = XmlDataSource1.GetXmlDocument().CreateElement("Category");
+        foreach (DictionaryEntry entry in e.NewValues) {
+            XmlAttribute attribute = XmlDataSource1.GetXmlDocument().CreateAttribute(entry.Key.ToString());
+            attribute.Value = GetValue(entry.Value);
+            node.Attributes.Append(attribute);
+        }
+        XmlDataSource1.GetXmlDocument().SelectSingleNode("Categories").AppendChild(node);
+        SaveXml(XmlDataSource1);
+
+        e.Cancel = true;
+
+        ASPxGridView grid = (ASPxGridView)sender;
+        grid.CancelEdit();
+    }
+    protected void ASPxGridView1_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
+        throw new InvalidOperationException("Data modifications are not allowed in online demos");
+
+        ASPxGridView grid = (ASPxGridView)sender;
+
+        int categoryID = int.Parse(e.Keys["CategoryID"].ToString());
+
+        XmlNode node = XmlDataSource1.GetXmlDocument().SelectSingleNode(string.Format("Categories/Category[@CategoryID='{0}']", categoryID));
+        if (node != null) {
+            foreach (DictionaryEntry entry in e.NewValues)
+                node.Attributes[entry.Key.ToString()].Value = entry.Value.ToString();
+
+            SaveXml(XmlDataSource1);
+        } else
+            grid.JSProperties["cpException"] = string.Format("The DataRow with keyValue = '{0}' was deleted by another user.", categoryID);
+
+        e.Cancel = true;
+
+        grid.CancelEdit();
+    }
+    protected void ASPxGridView1_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e) {
+        throw new InvalidOperationException("Data modifications are not allowed in online demos");
+
+        int categoryID = int.Parse(e.Keys["CategoryID"].ToString());
+
+        XmlNode node = XmlDataSource1.GetXmlDocument().SelectSingleNode(string.Format("Categories/Category[@CategoryID='{0}']", categoryID));
+        XmlNode parent = node.ParentNode;
+        parent.RemoveChild(node);
+
+        SaveXml(XmlDataSource1);
+
+        e.Cancel = true;
+
+        ASPxGridView grid = (ASPxGridView)sender;
+        grid.CancelEdit();
+    }
+    private string GetValue(object obj) {
+        return obj == null ? string.Empty : obj.ToString();
+    }
+    protected void SaveXml(XmlDataSource xmlDataSource) {
+        lock (lockObject) {
+            xmlDataSource.Save();
+        }
+    }
+    private void RestoreXMLFile(ASPxGridView grid) {
+        File.Copy(
+            Server.MapPath("~/App_Data/CategoriesBackUp.xml"),
+            Server.MapPath("~/App_Data/Categories.xml"),
+            true);
+        grid.DataBind();
+    }
+}
---
+++ VB/WebSite/Default.aspx
@@ -0,0 +1,68 @@
+<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+<%@ Register Assembly="DevExpress.Web.ASPxGridView.v9.3, Version=9.3.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxEditors.v9.3, Version=9.3.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dx" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+	<title></title>
+	<script language="javascript" type="text/javascript">
+		function OnEndCallbackEventHandler(s, e) {
+			if (s.cpException) {
+				alert(s.cpException.toString());
+				delete s.cpException;
+			}
+		}
+	</script>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+		<dx:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False"
+			ClientInstanceName="grid" DataSourceID="XmlDataSource1" KeyFieldName="CategoryID"
+			OnCellEditorInitialize="ASPxGridView1_CellEditorInitialize" OnCustomCallback="ASPxGridView1_CustomCallback"
+			OnRowInserting="ASPxGridView1_RowInserting" OnRowUpdating="ASPxGridView1_RowUpdating"
+			OnRowDeleting="ASPxGridView1_RowDeleting">
+			<Columns>
+				<dx:GridViewCommandColumn VisibleIndex="0">
+					<EditButton Visible="True">
+					</EditButton>
+					<DeleteButton Visible="True">
+					</DeleteButton>
+				</dx:GridViewCommandColumn>
+				<dx:GridViewDataTextColumn FieldName="CategoryID" VisibleIndex="1">
+					<EditFormSettings Visible="False" />
+				</dx:GridViewDataTextColumn>
+				<dx:GridViewDataTextColumn FieldName="CategoryName" VisibleIndex="2">
+				</dx:GridViewDataTextColumn>
+				<dx:GridViewDataTextColumn FieldName="Description" VisibleIndex="3">
+				</dx:GridViewDataTextColumn>
+			</Columns>
+			<Settings ShowStatusBar="Visible" />
+			<Templates>
+				<StatusBar>
+					<table cellpadding="0" cellspacing="0" width="100%">
+						<tr>
+							<td align="left">
+								<dx:ASPxButton ID="ASPxButton1" runat="server" Text="Add New Record" AutoPostBack="false">
+									<ClientSideEvents Click="function(s, e) { grid.AddNewRow(); }" />
+								</dx:ASPxButton>
+							</td>
+							<td align="right">
+								<dx:ASPxButton ID="ASPxButton2" runat="server" Text="Restore XML File" AutoPostBack="false">
+									<ClientSideEvents Click="function(s, e) { grid.PerformCallback('Restore'); }" />
+								</dx:ASPxButton>
+							</td>
+						</tr>
+					</table>
+				</StatusBar>
+			</Templates>
+			<ClientSideEvents EndCallback="OnEndCallbackEventHandler" />
+		</dx:ASPxGridView>
+	</div>
+	<asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/App_Data/Categories.xml">
+	</asp:XmlDataSource>
+	</form>
+</body>
+</html>
